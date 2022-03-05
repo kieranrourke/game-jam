e140c7207ece8e4916efb9d94fa3e75cf8f5ae91
orselane <77144245+orselane@users.noreply.github.com>
2022-03-05 11:51:39 -0500

    Ball V0.6
    Made ball a sprite (Uses spriteSheet class),
    added collision detection logic (that should be moved to game logic class later),
    added rebound to re-direct ball on planet/net collision (UNTESTED). Amortization could change to be based on mass/elasticity of matter (LATER).

--- ball.py
+++ ball.py
@@ -1,7 +1,7 @@
 """Ball class for space basketball"""
 
 __date__ = '3/4/22'
-__version__ = 'V0.4'
+__version__ = 'V0.5'
 __author__ = 'Nucleus team'
 
 import pygame
@@ -16,13 +16,11 @@ class Ball:
         self._ORANGE = (250,131,32)
         #set size (Deafult 20)
         self._radius = size
-        #set initial pos, spd, accel
+        #set initial pos
         self._pos = pygame.Vector2(ini_x,ini_y)
+        #set initial spd/accel to 0
         self._spd = pygame.Vector2(0,0)
         self._accel = pygame.Vector2(0,0)
-        #Set max spd/accel (absolute val. Set high to "uncap")
-        self._MAX_SPD = 3.5
-        self._MAX_ACCEL  = 1
         
         #Store planets
         self._planets = planets
@@ -42,53 +40,21 @@ class Ball:
         total_accel = pygame.Vector2() #0,0
     
         for planet in planets:
-            total_accel += planet.accel_applied(ball.get_pos(), ball.get_mass())
+            total_accel += planet.force_applied(ball.get_pos_tup(),
+                                              ball.get_mass())
         
         return total_accel
     
-    def _set_accel(self, new_accel: 'pygame.Vector2'):
-        """Sets acceleration within bounds defined by _MAX_ACCEL.
-        """
-        #0->x, 1 ->y. Sets for both cartesian coordinates
-        for i in range(0,2):
-            if new_accel[i] < -self._MAX_ACCEL:
-                #below -max, set to -max
-                self._accel[i] = -self._MAX_ACCEL
-            elif new_accel[i] > self._MAX_ACCEL:
-                #Above max, set to max
-                self._accel[i] = self._MAX_ACCEL
-            else:
-                #Within bounds, can set to given val
-                self._accel[i] = new_accel[i]
         
-    def _set_spd(self, new_spd: 'pygame.Vector2'):
-        """Sets speed within bounds defined by _MAX_SPD.
-        """    
-        #0->x, 1 ->y. Sets for both cartesian coordinates
-        for i in range(0,2):
-            if new_spd[i] < -self._MAX_SPD:
-                #below -max, set to -max
-                self._spd[i] = -self._MAX_SPD
-            elif new_spd[i] > self._MAX_SPD:
-                #Above max, set to max
-                self._spd[i] = self._MAX_SPD
-            else:
-                #Within bounds, can set to given val
-                self._spd[i] = new_spd[i]     
-                
-    def _set_pos(self, new_pos: 'pygame.Vector2'):
-        """Unsure if we'll bind it in the window, or if we'll cause a reset. 
-        TBD if we need this, keeping this here to remember it."""
-        raise NotImplementedError("_set_pos is not implemented yet")
-    
+    ##Might need to set bounds on acceleration/speed/position. Sometimes crashes
     def _update_pos(self, accel:'pygame.Vector2') -> None:
         """ Updates position based on speed of the ball (affected by planets). 
         Should be called on each game tick. 
         """   
         #Update accel (sum of forces) -> update spd -> update position
-        self._set_accel(accel)
-        self._set_spd(self._spd + self._accel)
-        self._pos += self._spd
+        self._accel += accel
+        self._spd += self._accel
+        self._pos += self._pos 
     
     def _draw(self) -> None:
         """Draws the ball on the screen. Should be called on each game tick.
@@ -116,6 +82,8 @@ class Ball:
     
     def get_pos_tup(self) -> tuple[int, int]:
         """ Returns the current position of the ball as a tuple 
-        in the form (x, y). Might remove later.
+        in the form (x, y).
         """
         return (self._pos.x, self._pos.y)
+    
+    
